02071d58cb2adc3d9086654da350eabda1a3bfa3
Frank Hsu <frankhsu523@gapp.nthu.edu.tw>
2018-06-24 00:35:22 +0800

    finished output data

--- bpmcf_lp_data.py
+++ bpmcf_lp_data.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+from copy import deepcopy
 import csv
 
 class LP:
@@ -17,7 +18,7 @@ class LP:
         self.param_b_v = None
         self.param_d = None
         self.param_exist_PK = None
-        self.param_c_p = None 
+        self.param_c_p = None
         self.param_exist_edge_PK = None
         self.param_exist_node_PK = None
 
@@ -30,9 +31,13 @@ class LP:
         self.set_param_c_e()
         self.set_param_b_v()
         self.set_param_d()
-        self.set_param_c_p()
         self.read_path(path_Name)
+        self.set_param_exist_PK()
+        self.set_param_c_p()
         self.set_param_exist_edge_PK()
+        self.set_param_exist_node_PK()
+
+        self.output_data(graph_Name)
 
     def read_graph(self, f_Name):
         status = None
@@ -69,27 +74,27 @@ class LP:
         nodes = self.G.nodes()
         nodes.sort(key=int)
         self.set_V = tuple(nodes)
-    
+
     def set_set_E(self):
         #initial the two dimensional matrix to 0
-        self.set_E = [['0' for i in range(len(self.set_V))] for y in range(len(self.set_V))]
+        self.set_E = [['-' for i in range(len(self.set_V))] for y in range(len(self.set_V))]
 
         for i, j in self.G.edges():
-            self.set_E[int(i)][int(j)] = '1'
+            self.set_E[int(i)][int(j)] = '+'
 
     def set_set_P(self, num):
-        self.set_P = tuple([i for i in range(num)])
-        
+        self.set_P = tuple([str(i) for i in range(num)])
+
     def set_set_K(self):
-        self.set_K = tuple([i for i in range(len(self.demand))])
+        self.set_K = tuple([str(i) for i in range(len(self.demand))])
         #print('set_K: ',self.set_K)
-   
+
     def set_param_c_e(self):
         self.param_c_e = [['0' for i in range(len(self.set_V))] for y in range(len(self.set_V))]
 
         for i,j,d in self.G.edges(data=True):
             self.param_c_e[int(i)][int(j)] = d['size']
-        
+
     def set_param_b_v(self):
         self.param_b_v = ['0' for i in range(len(self.set_V))]
 
@@ -101,21 +106,121 @@ class LP:
 
         for i, d in enumerate(list(self.demand)):
             self.param_d[i] = d[2]
-        
+
+    def set_param_exist_PK(self):
+        self.param_exist_PK = [['0' for i in range(len(self.set_P))] for j in range(len(self.set_K))]
+
+        k, p = (0, 0)
+        for i, path in enumerate(self.path):
+            if i > 0 and (path[0] != self.path[i-1][0] or path[-1] != self.path[i-1][-1]):
+                k += 1
+                p = 0
+
+            self.param_exist_PK[k][p] = '1'
+            p += 1
     def set_param_c_p(self):
         self.param_c_p = [[d[2] for i in self.set_P] for d in self.demand]
 
     def set_param_exist_edge_PK(self):
-        self.param_exist_edge_PK = []
-        for i, d in enumerate(self.path[:500]):
-            k, p = divmod(i,5)
-            if p == 0 and d[0] == self.path[i-1][0] and d[-1] == self.path[i-1][-1]:
-                pass
-            if d[0] == '1' and d[-1] == '0':
-                print(d)
-                #print(i,d)
-            #print(k,p)
+        initial_edge_matrix = [['0' for i in range(len(self.set_V))] for y in range(len(self.set_V))]
+        self.param_exist_edge_PK = [[ deepcopy(initial_edge_matrix) for i in range(len(self.set_P))] for j in range(len(self.set_K))]
 
+        k, p = (0, 0)
+        for i, path in enumerate(self.path):
+            if i > 0 and (path[0] != self.path[i-1][0] or path[-1] != self.path[i-1][-1]):
+                k += 1
+                p = 0
+
+            edge_matrix = self.param_exist_edge_PK[k][p]
+            for i, n in enumerate(path[:-1]):
+                edge_matrix[int(path[i])][int(path[i+1])] = '1'
+            p += 1
+        """
+        for i in self.param_exist_edge_PK:
+            for j in i:
+                for k in j:
+                    print(k)
+                print('_________________________________________________')
+        """
+
+    def set_param_exist_node_PK(self):
+        initial_node_matrix = [['0' for i in range(len(self.set_V))] for y in range(len(self.set_P))]
+        self.param_exist_node_PK = [ deepcopy(initial_node_matrix) for i in range(len(self.set_K))]
+
+        k, p = (0, 0)
+        for i, path in enumerate(self.path):
+            if i > 0 and (path[0] != self.path[i-1][0] or path[-1] != self.path[i-1][-1]):
+                k += 1
+                p = 0
+
+            node_matrix = self.param_exist_node_PK[k][p]
+            for v in path[:-1]:
+                node_matrix[int(v)] = '1'
+            p += 1
+
+        """
+        for i in self.param_exist_node_PK:
+            for j in i:
+                print(j)
+            print('_________________________________________________')
+        """
+
+    def output_data(self, graph_Name ):
+        f_Name = graph_Name.split('_')[0] + '.dat'
+
+        with open( f_Name, 'w' ) as f:
+            f.write("data;\n\n")
+
+            f.write("set V := {};\n\n".format(' '.join(self.set_V)))
+
+            f.write("set E :=	(*, *)	:	{} :=\n".format(' '.join(self.set_V)))
+            for i, v in enumerate(self.set_E):
+                f.write("			{:2d}	{}\n".format(i, ' '.join(v)))
+            f.write(";\n\n")
+
+            f.write("set P := {};\n\n".format(' '.join(self.set_P)))
+
+            f.write("set K := {};\n\n".format(' '.join(self.set_K)))
+
+            f.write("param c_e :=	[*,*]	:	{} :=\n".format(' '.join(self.set_V)))
+            for i, v in enumerate(self.param_c_e):
+                f.write("			{:2d}	{}\n".format(i, ' '.join(v)))
+            f.write(";\n\n")
+
+            f.write("param b_v :=	\n")
+            for i, v in enumerate(self.param_b_v):
+                f.write("		{:2d} {}\n".format(i, v))
+            f.write(";\n\n")
+
+            f.write("param d	:=	\n")
+            for i, v in enumerate(self.param_d):
+                f.write("		{:2d} {}\n".format(i, v))
+            f.write(";\n\n")
+
+            f.write("param exist_PK	:=	[*, *]	:	{} :=\n".format(' '.join(self.set_P)))
+            for i, k in enumerate(self.param_exist_PK):
+                f.write("			{:3d}	{}\n".format(i, ' '.join(k)))
+            f.write(";\n\n")
+
+            f.write("param c_p	:=	[*, *]	:	{} :=\n".format(' '.join(self.set_P)))
+            for i, k in enumerate(self.param_c_p):
+                f.write("			{:3d}	{}\n".format(i, ' '.join(k)))
+            f.write(";\n\n")
+
+            f.write("param exist_edge_PK	:=\n")
+            for index_k, k in enumerate(self.param_exist_edge_PK):
+                for index_p, p in enumerate(k):
+                    f.write("			[{}, {}, *, *]	:	{} :=\n".format(index_k,index_p,' '.join(self.set_V)))
+                    for i, v in enumerate(p):
+                        f.write("		{:2d} {}\n".format(i, ' '.join(v)))
+            f.write(";\n\n")
+
+            f.write("param exist_node_PK	:=\n")
+            for index_k, k in enumerate(self.param_exist_node_PK):
+                f.write("			[{}, *, *]	:	{} :=\n".format(index_k,' '.join(self.set_V)))
+                for i, v in enumerate(k):
+                    f.write("		{:2d} {}\n".format(i, ' '.join(v)))
+            f.write(";\n\n")
     def __str__(self):
         for i,j, d in self.G.edges(data=True):
             print(i,j,d)
